8aec74c2d31dab15f670217f3635d3f198a57e87
Zeu Capua <coding@zeu.dev>
2023-12-17 02:03:35 -0800

    🍻 start/stop timer duration

---
+++ src/components/TimerDuration.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+
+  export let start_time = Date.now();
+  let duration : number = 0;
+  let timer_interval : ReturnType<typeof setInterval>;
+
+  onMount(() => {
+    timer_interval = setInterval(() => {
+      duration = Math.floor((Date.now() - start_time) / 1000);
+    }, 1000);
+  });
+
+  onDestroy(() => { clearInterval(timer_interval); });
+</script>
+
+<p class="border border-orange-500 px-2 py-1">{duration} seconds</p>
