3cb0b43c36a85fa52293d285f70c56c618e875c9
Zeu Capua <coding@zeu.dev>
2023-12-21 12:27:56 -0800

    ✨ set and format duration

--- src/components/TimerDuration.svelte
+++ src/components/TimerDuration.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
+  import dayjs from "dayjs";
+  import duration from "dayjs/plugin/duration";
+  import localizedFormat from "dayjs/plugin/localizedFormat";
   import { onMount, onDestroy } from "svelte";
 
+  dayjs.extend(duration);
+  dayjs.extend(localizedFormat);
+
   export let start_time = Date.now();
-  let duration : number = 0;
+  let time_lapsed : number = 0;
   let timer_interval : ReturnType<typeof setInterval>;
 
   onMount(() => {
     timer_interval = setInterval(() => {
-      duration = Math.floor((Date.now() - start_time) / 1000);
+      time_lapsed = Math.floor((Date.now() - start_time) / 1000);
     }, 1000);
   });
 
-  onDestroy(() => { clearInterval(timer_interval); });
+  onDestroy(() => { 
+    clearInterval(timer_interval); 
+  });
 </script>
 
-<p class="border border-orange-500 px-2 py-1">{duration} seconds</p>
+<p class="border border-orange-500 px-2 py-1">
+  {dayjs.duration(time_lapsed, "seconds").format('HH:mm:ss')}
+</p>
